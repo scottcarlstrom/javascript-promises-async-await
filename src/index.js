3c653cc04e4341f597ad206974910e481104de1f
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-08-19 19:57:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 const movies = require('./data/movies.json'); 
+function getBooksAndMovies(){
+    Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
+}
