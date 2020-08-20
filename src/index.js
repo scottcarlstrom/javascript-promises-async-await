19bb31b4364ead873c18e5bc374c629dc457d1d1
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-08-19 19:35:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import { fetchWithTimeout } from './services';
 const movies = require('./data/movies.json'); 
 export function fetchMovies(){
     const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+const moviePromise = fetchMovies();
