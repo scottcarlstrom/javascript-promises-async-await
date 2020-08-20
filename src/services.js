6c3de219a146cd7ead24acb7ffaab4acf33fc556
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-08-19 19:49:51 -0700

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,3 +1,9 @@
 export function fetchWithTimeout(delay){
     return new Promise(resolve => setTimeout(resolve, delay));
 }
+export function fetchMovies(){
+    return fetch("./data/movies.json")
+        .then(response => response.json())
+        .then(movies => movies)
+        .catch(error => console.log(error));
+}
