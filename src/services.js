e6a1f6c2fc43f1dedad7a3e77563d0bb50a89377
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-08-23 11:20:07 -0700

    Update services.js

--- src/services.js
+++ src/services.js
@@ -16,5 +16,11 @@ export function fetchBooks(){
 }
 
 export async function asyncFetchMovies(){
-
+    try {
+        const response = await fetch("/data/movies.json");
+        const results = await response.json();
+        return results;
+    } catch (error) {
+        console.log(error);
+    }
 }
