b07c64fe60f9c1aa52abd7c6cf9f7bbd0ffab064
Scott Carlstrom <scottcarlstrom@hotmail.com>
2020-08-22 17:31:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,10 +16,12 @@ getBooksAndMoviesPromise.then(results => {
 function getBooksOrMoveis() {
     return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
-    .catch(error => console.log('Error waiting for the promise race', error));
+    .catch(error => {
+        console.log('Error waiting for the promise race', error);
+    });
 }
 
 const getBooksOrMoveisPromise = getBooksOrMoveis();
-getBooksOrMoveisPromise.then(results => {
+getBooksOrMoviesPromise.then(results => {
     console.log("getBooksOrMoviesPromise", results);
 });
